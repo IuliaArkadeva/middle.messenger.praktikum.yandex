264f2d605c1672a829b5a0c505e16f8560f9ed6b
Юлия Аркадьева <y.arkadeva@MacBook-Air-Ulia-2.local>
2022-10-16 15:03:26 +0500

    sprint_1

--- src/pages/profileEdit/index.js
+++ src/pages/profileEdit/index.js
@@ -1,9 +1,9 @@
 import Handlebars from 'handlebars';
 import tpl from './tmpl.hbs';
 import './style.scss';
-import header from '../../partials/layout_parts/header/'
-import decoreImage from '../../partials/components/simple/decoreImage/';
-import simpleForm from '../../partials/components/composite/simpleForm/';
-import textLine from '../../partials/components/simple/textLine/';
+import header from '../../partials/layout_parts/header'
+import decoreImage from '../../partials/components/simple/decoreImage';
+import simpleForm from '../../partials/components/composite/simpleForm';
+import textLine from '../../partials/components/simple/textLine';
 
 export default tpl;
